42e9a4b9e6900deaaf034ef44cd84e17a2dafa8f
sungset <greathwang92@gmail.com>
2026-09-18 09:09:56 +0900

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -3,6 +3,11 @@ import cors from "cors";
 import pinoHttpPkg from "pino-http";
 import { logger } from "./lib/logger.js";
 
+import usersRouter from "./routes/users.js";
+import gamesRouter from "./routes/games.js";
+import fundRouter from "./routes/fund.js";
+import expensesRouter from "./routes/expenses.js";
+
 const app = express();
 
 const pinoMiddleware = (pinoHttpPkg as any).default || pinoHttpPkg;
@@ -30,6 +35,11 @@ app.use(
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(usersRouter);
+app.use(gamesRouter);
+app.use(fundRouter);
+app.use(expensesRouter);
 
 // 타입을 명시하는 대신 매개변수를 any로 처리하여 타입 충돌 원천 차단
 app.get("/", (req: any, res: any) => {
